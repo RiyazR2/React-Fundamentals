44fb9153bb275f1f56b47eab99570e7f53853d9f
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2024-10-19 19:24:07 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,31 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import { TOGGLE_ICON } from "../utils/constants";
 import Sidebar from "../tasks/Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenuButton } from "../redux/appConfigSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleToggle = () => {
+    console.log("Clicked");
+    dispatch(toggleMenuButton());
+  };
+
+  const toggleMenu = useSelector((store) => store.toggleMenu.isToggleMenu);
   return (
     <>
       <div>
-        <div className="">
+        <div>
           <div>
             <img
               className="w-12 absolute mt-3 ml-4 rounded-lg cursor-pointer"
               src={TOGGLE_ICON}
               alt="ToggleProblem"
+              onClick={handleToggle}
             />
           </div>
-          <ul className=" p-6 flex justify-end text-white font-bold font-serif bg-[#000046] w-full">
+          <ul className=" p-6 flex justify-end text-white font-bold font-serif bg-[#000046] ">
             <li className="mx-6">
               <Link to={"/"}>Home</Link>
             </li>
@@ -28,11 +39,11 @@ const Header = () => {
           </ul>
         </div>
         <div className="grid grid-cols-12">
-          <div className=" col-span-2">
+          <div className={toggleMenu ? " col-span-2" : "hidden"}>
             <Sidebar />
           </div>
 
-          <div className={false ? " col-span-10" : " col-span-12"}>
+          <div className={toggleMenu ? " col-span-10" : " col-span-12"}>
             <Outlet />
           </div>
         </div>
