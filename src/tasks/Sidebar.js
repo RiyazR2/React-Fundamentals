398315c7ebde899b614cbf393156066a1afeee3d
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2024-10-19 19:25:19 +0530

    Added task

--- src/tasks/Sidebar.js
+++ src/tasks/Sidebar.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebarrrrrr</h1>
+      <ul>
+        <li>
+          <Link to={"/task1"}>Task1</Link>
+        </li>
+        <li>
+          <Link to={"/task2"}>Task2</Link>
+        </li>
+      </ul>
     </div>
   );
 };
