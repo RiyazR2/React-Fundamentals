147b831c3a0b9a41d9a57514922495616bdd08b7
Riyaz Pathan <riyazpathan193.rp@gmail.com>
2024-10-19 19:21:47 +0530

    Routes and redux added

--- src/App.js
+++ src/App.js
@@ -3,31 +3,45 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import appStore from "./redux/appStore";
+import { Provider } from "react-redux";
+import Task1 from "./tasks/Task1";
+import Task2 from "./tasks/Task2";
 
 const App = () => {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <Header />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        { path: "/contact", element: <Contact /> },
-      ],
-    },
-  ]);
   return (
     <>
       <div>
-        <RouterProvider router={appRouter} />
+        <Provider store={appStore}>
+          <RouterProvider router={appRouter} />
+        </Provider>
       </div>
     </>
   );
 };
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      { path: "/contact", element: <Contact /> },
+      {
+        path: "/task1",
+        element: <Task1 />,
+      },
+      {
+        path: "/task2",
+        element: <Task2 />,
+      },
+    ],
+  },
+]);
 export default App;
